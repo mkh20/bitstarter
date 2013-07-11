10b90811f901a3f3e8ac64c05912ad72543d59c3
Michael Hsueh <michael.k.hsueh@gmail.com>
2013-07-11 03:41:41 +0000

    trim string

--- web.js
+++ web.js
@@ -3,9 +3,9 @@ var fs = require( 'fs' );
 var express = require('express');
 
 var indexOut = fs.readFileSync('index.html', 'utf-8');
-var buffer  = new Buffer(indexOut.length());
+var buffer  = new Buffer(50);
 buffer.write(indexOut);
-var result = buffer.toString("utf-8");
+var result = buffer.toString("utf-8", 0, 27);
 console.log(result);
 
 

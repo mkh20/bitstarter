500b3a016eade83487a48a93c1abf87dbd75bea0
Michael Hsueh <michael.k.hsueh@gmail.com>
2013-07-11 03:24:53 +0000

    updated web.js to read text from index.html

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
+var fs = require( 'fs' );
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var buffer  = new Buffer(50);
+buffer.write(fs.readFileSync('index.html', 'utf-8'));
+var result = buffer.toString;
+console.log(result);
 
+
+
+var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+response.send(result);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+//fs.readFileSync(filename, [options])
+//buf.toString([encoding], [start], [end])

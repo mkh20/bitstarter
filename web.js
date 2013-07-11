c7afba5f87fe6f5afb9f9bdf9312827ca763a409
Michael Hsueh <michael.k.hsueh@gmail.com>
2013-07-11 04:37:52 +0000

    made web,js more concise

--- web.js
+++ web.js
@@ -2,13 +2,13 @@
 var fs = require( 'fs' );
 var express = require('express');
 
-var indexOut = fs.readFileSync('index.html', 'utf-8');
-var buffer  = new Buffer(50);
-buffer.write(indexOut);
-var result = buffer.toString("utf-8", 0, 27);
-console.log(result);
-
-
+//var indexOut = fs.readFileSync('index.html', 'utf-8');
+//var buffer  = new Buffer(50);
+//buffer.write(indexOut);
+//var result = buffer.toString("utf-8", 0, 27);
+//console.log(result);
+var buffer = new Buffer(fs.readFileSync('index.html', 'utf-8'), "utf-8");
+var result = buffer.toString("utf-8");
 
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {

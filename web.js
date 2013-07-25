8980a2014c15ee9af1c222b311959a2f6a793252
Michael Hsueh <michael.k.hsueh@gmail.com>
2013-07-25 05:45:20 +0000

    modified server to listen on port 8080

--- web.js
+++ web.js
@@ -17,7 +17,7 @@ response.send(result);
 response.end();
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
